f5b285a714f43f4f1aaf85b2a03c2e9d8d5d4465
Scott Falbo <scottfalboart@gmail.com>
2021-08-14 13:07:54 -0700

    Add project files.

---
+++ Tower-of-Hanoi/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tower_of_Hanoi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Tower-of-Hanoi/Tower.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tower_of_Hanoi
+{
+    public class Tower
+    {
+        public Pole[] Poles { get; set; }
+
+        public Tower(int discs)
+        {
+            Poles = new Pole[]
+                { 
+                    new Pole(discs),
+                    new Pole(discs),
+                    new Pole(discs)
+                };
+        }
+
+        public Pole[] SolveTowerArray(int discs) =>
+            SolveArray(discs, Poles[0], Poles[2], Poles[1]);
+
+
+        public void SolveTowerPrint(int discs) =>
+            SolvePrint(discs, Poles[0], Poles[2], Poles[1]);
+
+
+        public Pole[] SolveArray(int n, Pole startPole, Pole endPole, Pole centerPole)
+        {
+
+            return Poles;
+        }
+
+        public void SolvePrint(int n, Pole startPole, Pole endPole, Pole centerPole)
+        {
+
+        }
+
+
+    }
+
+    public class Pole
+    {
+        public int[] Tower { get; set; }
+
+        public Pole(int discs)
+        {
+            Tower = new int[discs];
+        }
+    }
+
+}
